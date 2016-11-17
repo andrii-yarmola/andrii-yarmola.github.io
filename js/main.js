e15b4dd95813f6938a674144d201f1ea4a048719
Andrii Yarmola <andrii.yarmola@psd2html.com>
2016-11-17 18:54:24 +0200

    3d menu

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
+
+    };
+
+});
+
+
+
+(function() {
+    'use strict';
+
+    var globalPerspectiveBox = document.querySelector('.global-perspective'),
+        winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    function setPerspective() {
+        var clientRect = globalPerspectiveBox.getBoundingClientRect(),
+            yPerspective = 100 * (winHeight / 2  - clientRect.top) / clientRect.height;
+
+        globalPerspectiveBox.style.perspectiveOrigin = '50% ' + yPerspective + '%';
+    }
+
+    function resizeHandler() {
+        winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        setPerspective();
+    }
+
+    function loadHandler() {
+        resizeHandler();
+    }
+
+    window.addEventListener('scroll', setPerspective);
+    window.addEventListener('resize', resizeHandler);
+    window.addEventListener('load',   loadHandler);
+}());
