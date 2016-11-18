ffffa1dafb78e93c436622b17c21442ae7eaab9d
Andrii Yarmola <andrii.yarmola@psd2html.com>
2016-11-18 15:18:19 +0200

    fixed hover-menu bug

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-$(document).ready(function() {
-    //Chrome Smooth Scroll
-    try {
-        $.browserSelector();
-        if($("html").hasClass("chrome")) {
-            $.smoothScroll();
-        }
-    } catch(err) {
-
-    };
-
-});
-
-
-
 (function() {
     'use strict';
 
